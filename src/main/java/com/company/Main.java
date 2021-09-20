ff2da56a5c23d110f077a18741f52981a06b87fa
Eivind Berger-Nilsen <eivind.bergernilsen@gmail.com>
2021-09-20 12:11:36 +0200

    Change main method signature.

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,11 +1,10 @@
 package com.company;
 
 import java.io.IOException;
-import java.net.ServerSocket;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) throws InterruptedException {
         try {
             EasySocket.start(new RegMatch(), Integer.parseInt(args[0]));
         } catch (IndexOutOfBoundsException ignored){
