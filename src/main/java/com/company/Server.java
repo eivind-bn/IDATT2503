6bc830776380a600fb07ad2ebd961f40c50c9652
Eivind Berger-Nilsen <eivind.bergernilsen@gmail.com>
2021-10-03 01:57:54 +0200

    Removed server interface.

--- src/main/java/com/company/Server.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-import java.io.IOException;
-import java.net.Socket;
-
-interface Server {
-    default void init(Receiver<String> input, Dispatcher<String> output) throws IOException{}
-    void execute(Receiver<String> input, Dispatcher<String> output) throws IOException;
-}
